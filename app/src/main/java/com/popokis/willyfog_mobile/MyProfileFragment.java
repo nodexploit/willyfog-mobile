ab34423ce1d2009502f9e7c7ca712bb53aaf0580
Nicolas Vargas <nvortega92@gmail.com>
2016-10-21 23:23:20 +0200

    #5 Vista perfil movil

--- app/src/main/java/com/popokis/willyfog_mobile/MyProfileFragment.java
+++ app/src/main/java/com/popokis/willyfog_mobile/MyProfileFragment.java
@@ -17,6 +17,9 @@ public class MyProfileFragment extends Fragment {
 
     protected TextView nameHeaderMain;
     protected TextView emailHeaderMain;
+    protected TextView userProfileName;
+    protected TextView userUniversityName;
+    protected TextView userProfileGrade;
 
     private final Gson gson = new Gson();
 
@@ -28,6 +31,7 @@ public class MyProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_my_profile, container, false);
         // Setting textViews
         NavigationView navigationView = (NavigationView) this.getActivity().findViewById(R.id.nav_view);
 
@@ -42,16 +46,30 @@ public class MyProfileFragment extends Fragment {
 
         userInfo = gson.fromJson(userInfoJson, UserInfo.class);
 
-        setProfileTextView(userInfo, navigationView.getHeaderView(0));
+        setProfileHeaderTextView(userInfo, navigationView.getHeaderView(0));
 
-        return inflater.inflate(R.layout.fragment_my_profile, container, false);
+
+        return setProfileTextView(userInfo, view);
     }
 
-    private void setProfileTextView(UserInfo userInfo, View view) {
+    private void setProfileHeaderTextView(UserInfo userInfo, View view) {
         nameHeaderMain          = (TextView) view.findViewById(R.id.nameHeaderMain);
         emailHeaderMain         = (TextView) view.findViewById(R.id.emailHeaderMain);
 
         nameHeaderMain.setText(userInfo.getName());
         emailHeaderMain.setText(userInfo.getEmail());
+    }
+
+    private View setProfileTextView(UserInfo userInfo, View view) {
+
+        userProfileName = (TextView) view.findViewById(R.id.user_profile_name);
+        userProfileGrade = (TextView) view.findViewById(R.id.user_profile_grade);
+        userUniversityName = (TextView) view.findViewById(R.id.user_university_name);
+
+        userProfileName.setText(userInfo.getName());
+        userProfileGrade.setText(userInfo.getDegree_name());
+        userUniversityName.setText(userInfo.getUniversity_name());
+
+        return view;
     }
 }
